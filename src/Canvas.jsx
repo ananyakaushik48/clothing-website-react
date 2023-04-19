4d41c73b64c4f3e8ef477ebc3fe1cb9d8c2202fb
Ananya Kaushik <ananyakaushik48@gmail.com>
2023-04-19 23:26:54 +0530

    added the 3D items

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,22 +1,34 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-export const App = ({ position = [-1, 0, 2.5], fov = 25}) => (
-<Canvas
-  camera={[position, fov]}
-  eventSource={document.getElementById('root')}
-  eventPrefix="client"
->
-  <Shirt/>
-  <OrbitControls/>
-</Canvas>
-)
+import { OrbitControls, useGLTF, Center, Environment } from "@react-three/drei";
+import { useRef } from "react";
+export const App = ({ position = [-0.8, 0, 2.5], fov = 20 }) => (
+  <Canvas
+    camera={[position, fov]}
+    eventSource={document.getElementById("root")}
+    eventPrefix="client"
+  >
+    <ambientLight intensity={0.5} />
+    <Environment preset="city" />
+    <Center>
+      <Shirt />
+    </Center>
+    <OrbitControls />
+  </Canvas>
+);
 
-
-function Shirt() {
+function Shirt(props) {
+  const { nodes, materials } = useGLTF("/shirt_baked.glb");
   return (
-    <mesh>
-      <boxGeometry args={[0.9,0.9,0.9]}  />
-    <meshNormalMaterial />
-    </mesh>
-  )
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        position={[0.42, 0, 0]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+    </group>
+  );
 }
+useGLTF.preload("/shirt_baked.glb");
